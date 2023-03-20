b486314e33a7df94db62735422dec169ad2b0318
Meini XUE <mxue@takima.fr>
2023-03-20 14:28:40 +0100

    Price class working.

--- src/test/java/io/takima/master3/store/article/ArticleDaoTest.java
+++ src/test/java/io/takima/master3/store/article/ArticleDaoTest.java
@@ -14,8 +14,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import java.util.Arrays;
+import java.util.regex.Pattern;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
